6137d7e589b00f2dfb0b8c4f3f638c4ef48707b7
Astagabdo Samira <117855051+Alim-Samira@users.noreply.github.com>
2026-02-09 12:05:13 +0000

    Initialisation du portfolio de Samira

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ function showPage(pageId) {
     if (target) target.classList.add('active');
 
     document.querySelectorAll('.nav-link').forEach(link => link.classList.remove('active'));
-    // Gestion du lien actif si l'événement existe
     if(event) event.currentTarget.classList.add('active');
     
     window.scrollTo({top: 0, behavior: 'smooth'});
@@ -13,6 +12,6 @@ function showPage(pageId) {
 function showName(input, targetId) {
     const target = document.getElementById(targetId);
     if (input.files.length > 0) {
-        target.innerHTML = `<i class="fas fa-check"></i> Fichier chargé : ${input.files[0].name}`;
+        target.innerHTML = `<i class="fas fa-check"></i> Prêt : ${input.files[0].name}`;
     }
 }
